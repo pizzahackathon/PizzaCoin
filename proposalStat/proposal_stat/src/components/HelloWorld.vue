00093d93c5744a5bf6e6fffce5f5ad875c0d8f86
Teerapat Tee <teerapat12345678@gmail.com>
2018-07-18 23:00:21 +0700

    mock card

---
+++ proposalStat/proposal_stat/src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+
+<template>
+<div>
+  <div class="columns is-multiline">
+    <div class="column is-4">
+      <div class="card">
+      <div class="card-image">
+    <figure class="image is-1by1">
+      <img src="http://www.eighteggs.com/katinrun/images/pic_1.jpg" alt="Placeholder image">
+    </figure>
+  </div>
+    <div class="card-content">
+      <div class="media">
+        <div class="media-content">
+          <p class="title is-5">โมจิด็อกเตอร์โค้กรีพอร์ทคอร์ส หน่อมแน้ม</p>
+        </div>
+      </div>
+      <div class="content">
+       เฝอแฟ้บ เอ็นจีโอช็อปเปอร์แรงใจปาสกาลแพตเทิร์น แมชีนแฮนด์ แซลมอน นางแบบคอลัมนิสต์ พาสเจอร์ไรส์วานิลลา เบญจมบพิตร ทอร์นาโดมาร์ก
+      </div>
+      <hr>
+       <div class="content">
+        <p class="title is-4">เป้าหมาย</p>
+        <progress class="progress is-success" value="60" max="100">60%</progress>
+      </div>
+       <footer class="card-footer">
+          <a href="#" class="card-footer-item">ผู้เข้าร่วม</a>
+          <a href="#" class="card-footer-item">ระยะทางรวม/กม.</a>
+          <a href="#" class="card-footer-item">เหลือเวลา/วัน</a>
+        </footer>
+      <a class="button is-link is-rounded is-fullwidth is-outlined">เข้าร่วมโครงการ</a>
+    </div>
+  </div>
+  </div>
+  </div>
+</div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ proposalStat/proposal_stat/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+      <div class="container">
+        <div class="columns is-mobile">
+          <HelloWorld />
+        </div>
+      </div>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import ProposalCard from '@/components/ProposalCard.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld,
+    ProposalCard
+  }
+}
+</script>
