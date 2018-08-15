e63b07dde012446d579c09d1ceb59f1d8b97494e
Nattapon Nimakul <totizy@gmail.com>
2018-08-15 23:52:23 +0700

    Add avatar for player

--- src/components/TeamProfile.vue
+++ src/components/TeamProfile.vue
@@ -1,18 +1,25 @@
 <template>
-    <div>
+    <div class="players-box">
         <div
-          class="main level is-mobile"
+          class="main level is-mobile player"
             v-for="member in team.members"
             :key="member.address"
               >
-          <div class="level-item has-text-centered">
+          <a :href="`${$store.state.system.etherscanPrefix}/address/${member.address}`" target="_blank">
+            <div class="level-item has-text-centered">
               <div>
-                  <div>{{ member.name }}</div>
+                <img :src="playerAvatarImage(member.address)" alt="">
+              </div>
+              <div>
+                  <div class="playerName">
+                    {{ member.name }}
+                  </div>
               </div>
           </div>
+          </a>
           <div class="level-item has-text-centered">
               <div>
-                  <div>{{ member.address }}</div>
+                  <!-- <div>{{ member.address }}</div> -->
               </div>
           </div>
           <div class="level-item has-text-centered" v-if="isLoggedIn && stateContract === 'Registration'">
@@ -56,6 +63,7 @@
 </template>
 <script>
 import { mapMutations, mapState, mapActions } from 'vuex'
+import Identicon from 'identicon.js'
 
 export default {
   data: () => ({
@@ -64,6 +72,30 @@ export default {
     pizzaCoin: null,
     pizzaCoinSymbol: '',
     userAddress: ''
+    // team: {
+    //   members: [
+    //     {
+    //       name: 'abc',
+    //       address: '0xfcee22fcc5607812db42371d9f75cf527e44718a'
+    //     },
+    //     {
+    //       name: 'abc',
+    //       address: '0x786f95663b1feaa429fe608dd51946356f9e6d54'
+    //     },
+    //     {
+    //       name: 'abc',
+    //       address: '0x950807aeaccb5e66dc09e9f99a7d559a880d8b14'
+    //     },
+    //     {
+    //       name: 'abc',
+    //       address: '0x69f6829b0a62c34a844e9a0a123dd4b1822a7bc5'
+    //     },
+    //     {
+    //       name: 'abc',
+    //       address: '0xdea6af9e0d4a7ac7e639b1c5751b58c165af590b'
+    //     }
+    //   ]
+    // }
   }),
   async mounted () {
     await this.loadPizzaCoinSymbol()
@@ -137,6 +169,10 @@ export default {
       } catch (error) {
         console.error(error)
       }
+    },
+    playerAvatarImage (address) {
+      let data = new Identicon(address, 120).toString()
+      return `data:image/png;base64,${data}`
     }
   }
 }
@@ -145,5 +181,23 @@ export default {
 .main {
     margin-top: 5em;
     font-size: 10px;
+}
+</style>
+<style scoped>
+.players-box {
+  display: flex;
+  flex-flow: row wrap;
+}
+.player {
+  display: inline-block;
+  margin: 10px 0 0 10px;
+  flex-grow: 1;
+  height: 100px;
+}
+.level-item {
+  display: block !important;
+}
+.playerName {
+  font-size: 22px;
 }
 </style>
