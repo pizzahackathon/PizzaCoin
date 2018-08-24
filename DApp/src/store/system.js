430cdb7293ba12b8c670295f9c5bc8007fe5f569
Nattapon Nimakul <totizy@gmail.com>
2018-08-25 04:07:10 +0700

    correct kovan infura web socket url

--- DApp/src/store/system.js
+++ DApp/src/store/system.js
@@ -1,7 +1,7 @@
 const state = {
   network: 'kovan',
   etherscanPrefix: 'https://kovan.etherscan.io',
-  ethereumNode: 'wss://kovan.infura.io/_ws', // Only websocker endpoint
+  ethereumNode: 'wss://kovan.infura.io/ws', // Only websocker endpoint
   pizzaCoinAddr: '0x2ae53961e6cad27ed6ef71c4b1e6021786d7846b',
   pizzaCoinStaffAddr: '0x2F1f50dc6F3D9B77FFd5b6C7769c2C28423099f2',
   pizzaCoinPlayerAddr: '0x377474c7afD40Dd75f4a6952CCB3bA69EF165159',
