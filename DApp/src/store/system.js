d21f3f8c031b16bbb75b91088421f003045a5c6f
Nattapon Nimakul <totizy@gmail.com>
2018-08-25 04:48:06 +0700

    update smart contract

--- DApp/src/store/system.js
+++ DApp/src/store/system.js
@@ -2,10 +2,10 @@ const state = {
   network: 'kovan',
   etherscanPrefix: 'https://kovan.etherscan.io',
   ethereumNode: 'wss://kovan.infura.io/ws', // Only websocker endpoint
-  pizzaCoinAddr: '0xe960868fa9e521f853ade9d5b40088b6387592f0',
-  pizzaCoinStaffAddr: '0x0C7FfBfAB626dDB6278CC1a20BA471981d1C8048',
-  pizzaCoinPlayerAddr: '0xfD4B7211Abb8b41bf45805A8F177FF4f22F52E36',
-  pizzaCoinTeamAddr: '0x156e3718c6F8EF104141f147FDd79775B7085783'
+  pizzaCoinAddr: '0xd5104240e1a449c2a7f22db74adc5d633a26748b',
+  pizzaCoinStaffAddr: '0x2e75cfb85acd1667d60e2325396CF5953cF205D7',
+  pizzaCoinPlayerAddr: '0x6658EBa89b9016248Ac70EFb959C7C96c5ee7e3A',
+  pizzaCoinTeamAddr: '0x5C4f4A508293a7EA5c0a8Ae7Dfaac3c60E4b2d09'
 }
 
 export default {
