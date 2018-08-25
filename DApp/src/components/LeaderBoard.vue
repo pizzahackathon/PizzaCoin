75a8f5fb5a2c14311a3420057859b3aa825ee25e
Nattapon Nimakul <totizy@gmail.com>
2018-08-25 10:03:19 +0700

    roll back reverser

--- DApp/src/components/LeaderBoard.vue
+++ DApp/src/components/LeaderBoard.vue
@@ -52,8 +52,6 @@ export default {
         }
         this.teamScore.push(team.score)
       })
-      this.teamNames = this.teamNames.reverse()
-      this.teamScore = this.teamScore.reverse()
     },
     initChartInstance (bgColor, borderColor) {
       this.leaderBoardData = {
--- DApp/src/store/system.js
+++ DApp/src/store/system.js
@@ -2,10 +2,10 @@ const state = {
   network: 'kovan',
   etherscanPrefix: 'https://kovan.etherscan.io',
   ethereumNode: 'wss://kovan.infura.io/ws', // Only websocker endpoint
-  pizzaCoinAddr: '0xd5104240e1a449c2a7f22db74adc5d633a26748b',
-  pizzaCoinStaffAddr: '0x2e75cfb85acd1667d60e2325396CF5953cF205D7',
-  pizzaCoinPlayerAddr: '0x6658EBa89b9016248Ac70EFb959C7C96c5ee7e3A',
-  pizzaCoinTeamAddr: '0x5C4f4A508293a7EA5c0a8Ae7Dfaac3c60E4b2d09'
+  pizzaCoinAddr: '0xed38bf5e334077bad86ba5d503fcb38dd30006d3',
+  pizzaCoinStaffAddr: '0x615ac1EAB45C4987e7A78DD19c18dAf598Ec4B88',
+  pizzaCoinPlayerAddr: '0x798Af328AF5D909C1dFed610EAc605a15d18Ea34',
+  pizzaCoinTeamAddr: '0xe6fd15D39A12e3BAAEf700a728C227ED37C8a618'
 }
 
 export default {
