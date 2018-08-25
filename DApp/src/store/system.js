31b67e9552663178e7c31b696c885e896f6c9d14
Nattapon Nimakul <totizy@gmail.com>
2018-08-25 10:08:44 +0700

    deploy production smart contract on kovan, let's enjoy

--- DApp/src/store/system.js
+++ DApp/src/store/system.js
@@ -2,10 +2,10 @@ const state = {
   network: 'kovan',
   etherscanPrefix: 'https://kovan.etherscan.io',
   ethereumNode: 'wss://kovan.infura.io/ws', // Only websocker endpoint
-  pizzaCoinAddr: '0xed38bf5e334077bad86ba5d503fcb38dd30006d3',
-  pizzaCoinStaffAddr: '0x615ac1EAB45C4987e7A78DD19c18dAf598Ec4B88',
-  pizzaCoinPlayerAddr: '0x798Af328AF5D909C1dFed610EAc605a15d18Ea34',
-  pizzaCoinTeamAddr: '0xe6fd15D39A12e3BAAEf700a728C227ED37C8a618'
+  pizzaCoinAddr: '0x76030b8f0e6e938afabe7662ec248f2b7815e6bb',
+  pizzaCoinStaffAddr: '0xEa1E67465b688Ea1b30856F55AcD77af43376d01',
+  pizzaCoinPlayerAddr: '0x785A811Ad43c733B0FdDd8113E8478bc2AEd02e0',
+  pizzaCoinTeamAddr: '0x216C611001b2e8B6ff2cf51C5e9EB39ABE558E35'
 }
 
 export default {
